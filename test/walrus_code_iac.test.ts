d613e379790ba129deb1ad570511027da66914d3
Martin <mart256@gmail.com>
2022-03-27 15:06:34 +0200

    Deleted sample code.

--- test/walrus_code_iac.test.ts
+++ test/walrus_code_iac.test.ts
@@ -4,14 +4,4 @@ import * as WalrusCodeIac from '../lib/walrus_code_iac-stack';
 
 test('SQS Queue and SNS Topic Created', () => {
   const app = new cdk.App();
-  // WHEN
-  const stack = new WalrusCodeIac.WalrusCodeIacStack(app, 'MyTestStack');
-  // THEN
-
-  const template = Template.fromStack(stack);
-
-  template.hasResourceProperties('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
-  });
-  template.resourceCountIs('AWS::SNS::Topic', 1);
 });
