b59e334d539974165c78f4b9e72ce4fb3ee94480
Martin <mart256@gmail.com>
2022-03-27 16:15:58 +0200

    Using env variables to pass account and region

--- bin/walrus_code_iac.ts
+++ bin/walrus_code_iac.ts
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { WalrusCodeIacStack } from '../lib/walrus_code_iac-stack';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = new cdk.App();
-new WalrusCodeIacStack(app, 'WalrusCodeIacStack');
+new WalrusCodeIacStack(app, 'WalrusCodeIacStack', {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION
+    }
+});
--- lib/walrus_code_iac-stack.ts
+++ lib/walrus_code_iac-stack.ts
@@ -1,17 +1,24 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import {Stack, StackProps} from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import { Construct } from 'constructs';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import {Construct} from 'constructs';
 
 export class WalrusCodeIacStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    const setupScript = ec2.UserData.forLinux();
-    setupScript.addCommands('echo "Hello world from launch template in CDK!"');
+        const setupScript = ec2.UserData.forLinux();
+        setupScript.addCommands('echo "Hello world from launch template in CDK!"');
 
-    const launchTemplate = new ec2.LaunchTemplate(this, 'myLaunchTemplate', {
-      userData: setupScript,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO)
-    });
-  }
+        const launchTemplate = new ec2.LaunchTemplate(this, 'myLaunchTemplate', {
+            userData: setupScript,
+            instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO)
+        });
+
+        const vpc = ec2.Vpc.fromLookup(this, 'vpc-34818c4e', {isDefault: true});
+
+        const alb = new elbv2.ApplicationLoadBalancer(this, 'myAlb', {vpc});
+        const listener = alb.addListener('listener', {port: 80});
+        // listener.addTargets('target', {port: 80});
+    }
 }
