faace7d51bde11fae77b1aed1f2fd46695255978
Martin <mart256@gmail.com>
2022-03-27 15:25:26 +0200

    Defined the first version of the launch template

--- lib/walrus_code_iac-stack.ts
+++ lib/walrus_code_iac-stack.ts
@@ -1,8 +1,17 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
 export class WalrusCodeIacStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const setupScript = ec2.UserData.forLinux();
+    setupScript.addCommands('echo "Hello world from launch template in CDK!"');
+
+    const launchTemplate = new ec2.LaunchTemplate(this, 'myLaunchTemplate', {
+      userData: setupScript,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO)
+    });
   }
 }
